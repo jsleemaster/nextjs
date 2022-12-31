fde1dd0e9952388056a5e224924885f57cedb478
jsleemaster <roeowo123@naver.com>
2022-12-31 17:39:16 +0900

    feat: dateCheck

--- pages/dateCheck.js
+++ pages/dateCheck.js
@@ -1,12 +1,21 @@
 import styles from '../styles/Home.module.css'
+import dateStyles from '../styles/dateCheck.module.css'
+import dayjs from 'dayjs'
+import React from 'react'
 
 export default function Home() {
+  const [date, setDate] = React.useState(dayjs().format('YYYY-MM-DD HH:mm:ss'));
+  React.useEffect(() => {
+    setInterval(() => {
+      setDate(dayjs().format('YYYY-MM-DD HH:mm:ss'))
+    }, 1000);
+  }, [])
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-
+        <span className={dateStyles.date}>{date}</span>
       </main>
-
     </div>
   )
 }
