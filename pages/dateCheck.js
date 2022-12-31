54a476abdf1ddb0b5f35e44865e687e9336b3c94
jsleemaster <roeowo123@naver.com>
2022-12-31 18:30:47 +0900

    feat: 남은시간체크

--- pages/dateCheck.js
+++ pages/dateCheck.js
@@ -1,20 +1,30 @@
 import styles from '../styles/Home.module.css'
 import dateStyles from '../styles/dateCheck.module.css'
 import dayjs from 'dayjs'
-import React from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
-  const [date, setDate] = React.useState(dayjs().format('YYYY-MM-DD HH:mm:ss'));
-  React.useEffect(() => {
+  const [date, setDate] = useState(dayjs().format('YYYY-MM-DD HH:mm:ss.SSS'));
+  const reach = '2023-01-01 00:00:00'
+  const [reachDate, setReachDate] = useState(dayjs(reach).format('YYYY-MM-DD HH:mm:ss.SSS'));
+  const [diffDate, setDiffDate] = useState(0)
+
+  useEffect(() => {
     setInterval(() => {
       setDate(dayjs().format('YYYY-MM-DD HH:mm:ss'))
     }, 1000);
   }, [])
 
+  useEffect(() => {
+    setDiffDate(dayjs(date).diff(reach));
+    setReachDate(dayjs(reach).subtract(diffDate).format("HH:mm:ss"))
+  }, [date])
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <span className={dateStyles.date}>{date}</span>
+        <span className={dateStyles.reach_date}>남은 시간 {reachDate}</span>
       </main>
     </div>
   )
