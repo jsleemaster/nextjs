1892103c47ea1b1c70540a5ead69cb6a7681d0bc
jsleemaster <roeowo123@naver.com>
2023-01-07 16:11:25 +0900

    feat: date추가

--- pages/dateCheck/index.tsx
+++ pages/dateCheck/index.tsx
@@ -1,32 +1,42 @@
-import styles from '../styles/Home.module.css'
-import dateStyles from '../styles/dateCheck.module.css'
-import dayjs from 'dayjs'
-import { useEffect, useState, } from 'react'
+import dateStyles from "styles/dateCheck.module.css";
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
 
-export default function Home() {
-  const reach = dayjs().add(1, 'year').format('YYYY-MM-DD HH:mm:ss')
-  console.log(reach, '<<<')
-  const [date, setDate] = useState(dayjs().format('YYYY-MM-DD HH:mm:ss.SSS'));
-  const [reachDate, setReachDate] = useState(dayjs(reach).format('YYYY-MM-DD HH:mm:ss.SSS'));
-  const [diffDate, setDiffDate] = useState(0)
+export default function DateCheck() {
+  const nextYear = dayjs()
+    .add(1, "year")
+    .startOf("year")
+    .format("YYYY-MM-DD HH:mm:ss.SSS");
+
+  const [date, setDate] = useState(dayjs().format("YYYY-MM-DD HH:mm:ss.SSS"));
+  const [nextYearTime, setnextYearTime] = useState(
+    dayjs(nextYear).format("YYYY-MM-DD HH:mm:ss.SSS")
+  );
+  const [diffDate, setDiffDate] = useState(0);
 
   useEffect(() => {
     setInterval(() => {
-      setDate(dayjs().format('YYYY-MM-DD HH:mm:ss'))
-    }, 1000);
-  }, [])
+      setDate(dayjs().format("YYYY-MM-DD HH:mm:ss.SSS"));
+    }, 100);
+  }, []);
 
   useEffect(() => {
-    setDiffDate(dayjs(date).diff(reach));
-    setReachDate(dayjs(reach).subtract(diffDate).format("HH:mm:ss"))
-  }, [date])
+    setDiffDate(dayjs(date).diff(nextYear));
+    setnextYearTime(dayjs(nextYear).subtract(diffDate).format("HH:mm:ss.SSS"));
+  }, [date]);
 
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
+    <div className={dateStyles.container}>
+      <main className={dateStyles.main}>
         <span className={dateStyles.date}>{date}</span>
-        {diffDate < 0 && <span className={dateStyles.reach_date}>남은 시간 {reachDate}</span>}
+        {diffDate < 0 && (
+          <span className={dateStyles.next_year_date}>
+            {dayjs(nextYear).format("YYYY")}년 까지 남은 시간{" "}
+            <b>D-{dayjs(nextYear).diff(date, "day")}</b>
+            {nextYearTime}
+          </span>
+        )}
       </main>
     </div>
-  )
+  );
 }
