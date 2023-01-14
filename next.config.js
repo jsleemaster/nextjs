3fe515712b5accc3cb0f1d5cecf7ee27c9a4252c
jsleemaster <roeowo123@naver.com>
2023-01-14 09:10:54 +0900

    feat: config 추가

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  pageExtensions: ['jsx', 'js', 'tsx', 'ts'],
+  experimental: { appDir: true },
+  webpack(config) {
+    config.experiments = { ...config.experiments, topLevelAwait: true }
+    return config
+  },
+
 }
 
 module.exports = nextConfig
