30c47a3e6177a9bdc8f96ff6f1799f6c29f49542
jsleemaster <roeowo123@naver.com>
2023-01-28 15:24:25 +0900

    feat: fetch 테스트

--- pages/anyQuestion/index.tsx
+++ pages/anyQuestion/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
-import { useState, FormEvent } from "react";
-import { useAiFetch } from "./useFetch";
+import { useState, FormEvent, Suspense } from "react";
+import { useAiFetch } from "hooks/anyQuestion/useFetch";
 
 export default function AnyQuestion() {
   const [question, setQuestion] = useState<string>("");
@@ -8,8 +8,10 @@ export default function AnyQuestion() {
 
   const GetAnswer = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    console.log(question, "이거 찍ㅎ");
     try {
       const res = await useAiFetch("openAi", question);
+      console.log(res);
       return res.json();
     } catch (error) {
       return error;
@@ -21,17 +23,19 @@ export default function AnyQuestion() {
       <Head>
         <title>무엇이든 물어보세요!</title>
       </Head>
-      <div>
-        <form onSubmit={GetAnswer}>
-          <input
-            type="text"
-            placeholder="질문을 입력해주세요."
-            value={question}
-            onChange={(e) => setQuestion(e.target.value)}
-          />
-          <button type="submit"> 확인 </button>
-        </form>
-      </div>
+      <Suspense fallback={<div>로딩중...</div>}>
+        <div>
+          <form onSubmit={GetAnswer}>
+            <input
+              type="text"
+              placeholder="질문을 입력해주세요."
+              value={question}
+              onChange={(e) => setQuestion(e.target.value)}
+            />
+            <button type="submit"> 확인 </button>
+          </form>
+        </div>
+      </Suspense>
     </>
   );
 }
--- pages/api/openAi/index.ts
+++ pages/api/openAi/index.ts
@@ -1,10 +1,18 @@
-import * as Config from "config";
-
-function generatePrompt(animal: string) {
-  // const capitalizedAnimal =
-  //   animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  // return capitalizedAnimal;
+import { NextApiRequest, NextApiResponse } from "next";
+interface Prompt {
+  model: String;
+  temperature: Number;
+  max_tokens: Number;
+  top_p: Number;
+  frequency_penalty: Number;
+  presence_penalty: Number;
+  data: String;
 }
+
+const generatePrompt = (data: Prompt) => {
+  console.log(data, "<<<<<<<<");
+  return data;
+};
 type Method = "GET" | "POST" | "PUT" | "DELETE";
 type Http = [Method, string];
 
@@ -12,12 +20,12 @@ interface EndPoint {
   openAi: Http;
 }
 
-const endPoint: EndPoint = {
-  openAi: ["POST", "/openAi"],
-};
-
-export default function handler(req, res) {
-  // endPoint[req.method]
-
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const requestMethod = req.method;
+  const body = JSON.parse(req.body);
+  switch (requestMethod) {
+    case "POST":
+      return res.status(200).json(generatePrompt(body));
+  }
   return res.status(404).json({ message: "Not Found" });
 }
