905e1c0a491c0be131293ee0ab4268f57129b353
jsleemaster <roeowo123@naver.com>
2023-04-16 17:56:55 +0900

    refactor style 분리

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -1,59 +1,19 @@
 import { useCallback } from "react";
-import { useAppDispatch, useAppSelector } from "hooks/reduxHooks";
+import { useAppDispatch } from "hooks/reduxHooks";
 import { toggleTheme } from "store/actions/theme";
 
-import { useTheme, styled, alpha } from "@mui/material/styles";
+/* mui */
+import { useTheme } from "@mui/material/styles";
 import WbSunnyIcon from "@mui/icons-material/WbSunny";
 import ModeNightIcon from "@mui/icons-material/ModeNight";
 import IconButton from "@mui/material/IconButton";
-
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import InputBase from "@mui/material/InputBase";
 import SearchIcon from "@mui/icons-material/Search";
 
-const Search = styled("div")(({ theme }) => ({
-  "position": "relative",
-  "borderRadius": theme.shape.borderRadius,
-  "backgroundColor": alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  "marginLeft": 0,
-  "width": "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(1),
-    width: "auto",
-  },
-}));
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  "color": "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      "width": "12ch",
-      "&:focus": {
-        width: "20ch",
-      },
-    },
-  },
-}));
+/* style */
+import { Search, StyledInputBase, SearchIconWrapper } from "./Header.style";
 
 const Header = () => {
   const dispatch = useAppDispatch();
