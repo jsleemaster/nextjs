879f97c7c6e0aa5557abd850d9cd1d4059adad31
jsleemaster <roeowo123@naver.com>
2023-04-23 15:41:49 +0900

    refactor 예외처리

--- components/layouts/Sidebar.tsx
+++ components/layouts/Sidebar.tsx
@@ -7,41 +7,44 @@ import Typography from "@mui/material/Typography";
 const SideBar = () => {
   const Items = useAppSelector((state) => state.portFolio.filterItems);
   const router = useRouter();
+
+  if (!Items) {
+    return (
+      <>
+        {[1, 2, 3].map((index) => (
+          <Skeleton
+            key={index}
+            variant="rounded"
+            width={"100%"}
+            height={"100%"}
+            animation="wave"
+          />
+        ))}
+      </>
+    );
+  }
+  if (Items.length === 0) {
+    return (
+      <SideUl>
+        <Sideli>
+          <Typography> 데이터가 없습니다.</Typography>
+        </Sideli>
+      </SideUl>
+    );
+  }
+
   return (
     <SideUl>
-      {Items ? (
-        Items.map((portFolio) => {
-          return (
-            <Sideli
-              key={portFolio.id}
-              onClick={() => router.push(`/${portFolio.href}`)}
-            >
-              <Typography>{portFolio.name}</Typography>
-            </Sideli>
-          );
-        })
-      ) : (
-        <>
-          <Skeleton
-            variant="rounded"
-            width={"100%"}
-            height={"100%"}
-            animation="wave"
-          />
-          <Skeleton
-            variant="rounded"
-            width={"100%"}
-            height={"100%"}
-            animation="wave"
-          />
-          <Skeleton
-            variant="rounded"
-            width={"100%"}
-            height={"100%"}
-            animation="wave"
-          />
-        </>
-      )}
+      {Items.map((portFolio) => {
+        return (
+          <Sideli
+            key={portFolio.id}
+            onClick={() => router.push(`/${portFolio.href}`)}
+          >
+            <Typography>{portFolio.name}</Typography>
+          </Sideli>
+        );
+      })}
     </SideUl>
   );
 };
