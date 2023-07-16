fd7f865e082fb53cbc05a5474600c99f03e10659
jsleemaster <roeowo123@naver.com>
2023-07-16 15:09:56 +0900

    add nextjs config - image domain

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ const nextConfig = {
   experimental: { appDir: true },
   output: "standalone",
   poweredByHeader: false,
+  images: {
+    domains: ["https://nextjs-lahrlvbi4-jsleemaster.vercel.app"],
+  },
 };
 
 module.exports = nextConfig;
