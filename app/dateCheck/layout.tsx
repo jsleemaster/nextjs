7aec0052eb2951125ff4d4d85d04c6e4cb202cc6
jsleemaster <roeowo123@naver.com>
2023-07-16 14:35:45 +0900

    feat meta data

--- app/dateCheck/layout.tsx
+++ app/dateCheck/layout.tsx
@@ -4,6 +4,16 @@ import { ReactNode } from "react";
 
 export const metadata: Metadata = {
   title: "날짜 계산앱",
+  description: "날짜 계산, 내년까지 얼마나 남았을까?",
+  keywords: "dateCacalutor, 날짜계산, 내년, 날짜, 날짜에 일더하기",
+  authors: { url: "", name: "jsleemaster" },
+  openGraph: {
+    siteName: "jsleemaster",
+    title: "날짜 계산앱",
+    description: "날짜 계산, 내년까지 얼마나 남았을까?",
+    type: "website",
+    url: "",
+  },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
