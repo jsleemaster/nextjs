5e7c76bc0dc25ccf1bee6a423a4b4903a4136b53
jsleemaster <roeowo123@naver.com>
2023-10-15 16:00:08 +0900

    [추가] famous

---
+++ app/famous/layout.tsx
@@ -0,0 +1,16 @@
+import Providers from "store/provider";
+import { Metadata } from "next";
+import { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "하루 명언",
+  description: "Jsleemaster App",
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+  },
+};
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return <Providers>{children}</Providers>;
+}
---
+++ app/famous/page.tsx
@@ -0,0 +1,5 @@
+const Famous = () => {
+  <></>;
+};
+
+export default Famous;
