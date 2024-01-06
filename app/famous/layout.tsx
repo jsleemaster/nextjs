c5331653b6f850aded1a9e1c94290caa46adb998
jsleemaster <roeowo123@naver.com>
2024-01-06 14:18:00 +0900

    viewport 변경 - 해당 부분 14부터 변경된 것으로 보임

--- app/famous/layout.tsx
+++ app/famous/layout.tsx
@@ -6,10 +6,6 @@ import Providers from "store/provider";
 export const metadata: Metadata = {
   title: "명언",
   description: "Jsleemaster App",
-  viewport: {
-    width: "device-width",
-    initialScale: 1,
-  },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
