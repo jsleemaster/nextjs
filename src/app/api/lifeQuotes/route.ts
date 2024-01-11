4a904230b8d93282b0c3789f579224b62828b319
jsleemaster <roeowo123@naver.com>
2024-01-11 15:57:45 +0900

    test

--- src/app/api/lifeQuotes/route.ts
+++ src/app/api/lifeQuotes/route.ts
@@ -4,30 +4,24 @@ export const notionToken = process.env.NEXT_PUBLIC_NOTION_FAMOUS_TOKEN;
 export const notionQuotesId = process.env.NEXT_PUBLIC_NOTION_FAMOUS_ID;
 
 export async function GET() {
-  let errMessage = "";
-  try {
-    const res = await fetch(
-      `https://api.notion.com/v1/databases/${notionQuotesId}/query`,
-      {
-        method: "POST",
-        headers: {
-          "Authorization": `Bearer ${notionToken}`,
-          "Notion-Version": "2022-06-28",
-        },
-      }
-    );
-    const data = await res.json();
-    const quotesData: LifeQuotesType[] = [];
-    data.results.forEach((quotes, idx) => {
-      quotesData.push({
-        author: quotes.properties.태그.select.name,
-        title: quotes.properties.이름.title[0].plain_text,
-        idx,
-      });
+  const res = await fetch(
+    `https://api.notion.com/v1/databases/${notionQuotesId}/query`,
+    {
+      method: "POST",
+      headers: {
+        "Authorization": `Bearer ${notionToken}`,
+        "Notion-Version": "2022-06-28",
+      },
+    }
+  );
+  const data = await res.json();
+  const quotesData: LifeQuotesType[] = [];
+  data.results.forEach((quotes, idx) => {
+    quotesData.push({
+      author: quotes.properties.태그.select.name,
+      title: quotes.properties.이름.title[0].plain_text,
+      idx,
     });
-    return Response.json({ data: quotesData });
-  } catch (err) {
-    errMessage = err;
-    return Response.json({ data: { "test": err, "hi": "hi" }, status: 404 });
-  }
+  });
+  return Response.json({ data: quotesData, test: data, test2: res });
 }
