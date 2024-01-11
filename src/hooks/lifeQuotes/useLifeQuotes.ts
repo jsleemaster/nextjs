18fe96ddc883fd1987f9170c3422644d69a13111
jsleemaster <roeowo123@naver.com>
2024-01-11 15:29:43 +0900

    [move] 폴더 구조변경

--- src/hooks/lifeQuotes/useLifeQuotes.ts
+++ src/hooks/lifeQuotes/useLifeQuotes.ts
@@ -23,7 +23,7 @@ export default () => {
   };
   const getData = async () => {
     try {
-      const res = await fetch("/lifeQuotes/api");
+      const res = await fetch("/api/lifeQuotes/");
       const { data } = await res.json();
       getNewMessage(data);
     } catch (error) {
