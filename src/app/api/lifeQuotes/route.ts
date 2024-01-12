cd50217f0a3f311b561aa2dfe919d3f7c2f4d4b6
jsleemaster <roeowo123@naver.com>
2024-01-12 11:53:39 +0900

    데이터 테스트

--- src/app/api/lifeQuotes/route.ts
+++ src/app/api/lifeQuotes/route.ts
@@ -19,8 +19,8 @@ export async function GET() {
   if (data.results && data.results.length > 0) {
     data.results.forEach((quotes, idx) => {
       quotesData.push({
-        author: quotes.properties,
-        title: quotes.properties,
+        author: quotes.properties.author?.Author?.select?.name,
+        title: quotes.properties.title?.title.title[0]?.plain_text,
         idx,
       });
     });
