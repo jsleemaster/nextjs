e48366ddb525dba950359b0872a9ed40cf3d22db
jsleemaster <roeowo123@naver.com>
2024-01-14 19:23:59 +0900

    [add] CSS

---
+++ src/components/lifeQuotes/Message.css.ts
@@ -0,0 +1,18 @@
+import { style } from "@vanilla-extract/css";
+
+export const quotoesBox = style({
+  color: "black",
+  padding: 10,
+  display: "flex",
+  height: "100%",
+  justifyContent: "center",
+  flexDirection: "column",
+  alignItems: "center",
+  borderColor: "black",
+});
+export const quotesTitle = style({
+  fontSize: 16,
+});
+export const quotesAuthor = style({
+  fontSize: 14,
+});
