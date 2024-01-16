4617f96fad377cd4fefa8ba9507a40d123446798
jsleemaster <roeowo123@naver.com>
2024-01-16 12:55:55 +0900

    [fix] 랜더링 이슈 [add] useMounted 훅추가

--- src/app/lifeQuotes/page.tsx
+++ src/app/lifeQuotes/page.tsx
@@ -3,35 +3,39 @@
 import LifeQuotesMessage from "components/lifeQuotes/Message";
 import useLifeQuotes from "hooks/lifeQuotes/useLifeQuotes";
 import useNotification from "hooks/lifeQuotes/useNotification";
+import useMouted from "hooks/useMouted";
 
 import * as styles from "./page.css";
 export default function LifeQuotes() {
+  const mounted = useMouted();
   const { notificationErrorMessage, notificationPermission } =
     useNotification();
   const { lifeQuotes, setTime, time } = useLifeQuotes();
   return (
-    <>
-      <section className={styles.section}>
-        <label htmlFor="time" className={styles.label}>
-          시간 선택
-        </label>
-        <select
-          onChange={(e) => setTime(Number(e.target.value))}
-          id="time"
-          className={styles.select}
-          value={time}
-        >
-          <option value={900000}>10분</option>
-          <option value={1800000}>30분</option>
-          <option value={3600000}>1시간</option>
-        </select>
-      </section>
-      {notificationPermission !== "granted" && (
-        <section>{notificationErrorMessage}</section>
-      )}
-      <section className={styles.messageSection}>
-        <LifeQuotesMessage lifeQuotes={lifeQuotes} />
-      </section>
-    </>
+    mounted && (
+      <>
+        <section className={styles.section}>
+          <label htmlFor="time" className={styles.label}>
+            시간 선택
+          </label>
+          <select
+            onChange={(e) => setTime(Number(e.target.value))}
+            id="time"
+            className={styles.select}
+            value={time}
+          >
+            <option value={900000}>10분</option>
+            <option value={1800000}>30분</option>
+            <option value={3600000}>1시간</option>
+          </select>
+        </section>
+        {notificationPermission !== "granted" && (
+          <section>{notificationErrorMessage}</section>
+        )}
+        <section className={styles.messageSection}>
+          <LifeQuotesMessage lifeQuotes={lifeQuotes} />
+        </section>
+      </>
+    )
   );
 }
--- src/components/lifeQuotes/Message.tsx
+++ src/components/lifeQuotes/Message.tsx
@@ -5,16 +5,14 @@ import * as styles from "./Message.css";
 const Message = ({ lifeQuotes }) => {
   const [loading, setLoading] = useState(true);
   useEffect(() => {
-    if (lifeQuotes) {
-      setLoading(false);
-      setTimeout(() => {
-        setLoading(true);
-      }, 1000);
-    }
+    setLoading(false);
+    setTimeout(() => {
+      setLoading(true);
+    }, 1000);
   }, [lifeQuotes]);
   return (
     <article
-      className={`${styles.quotoesBox} ${loading ? styles.scrollActive : ""}`}
+      className={`${styles.quotoesBox} ${!loading ? styles.scrollActive : ""}`}
     >
       <span className={styles.quotesTitle}>
         {lifeQuotes && lifeQuotes.title}
