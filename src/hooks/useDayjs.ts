f7b5e48c5e1b70f40028d120266ff7a2c0810635
jsleemaster <roeowo123@naver.com>
2024-01-22 12:33:39 +0900

    [fix] dayjs year

--- src/hooks/useDayjs.ts
+++ src/hooks/useDayjs.ts
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 
 export const nowDateTime = () => dayjs().format("YYYY-MM-DD HH:mm:ss");
-export const year = (date?: string) => dayjs(date ?? "").format("YYYY");
+export const year = (date: string | undefined = undefined) =>
+  dayjs(date).format("YYYY");
 export const diffDay = (prev: string, diff: string) =>
   dayjs(prev).diff(diff, "day");
