4abb882fcd16727ffd9e7bae7993f9c57023553e
jsleemaster <roeowo123@naver.com>
2024-01-26 12:48:52 +0900

    [fix] css

--- src/styles/dateCheck/input.css.ts
+++ src/styles/dateCheck/input.css.ts
@@ -2,6 +2,16 @@ import { style } from "@vanilla-extract/css";
 import { responsiveStyle } from "styles/responsive.css";
 export const dateInputContainer = style({
   display: "flex",
+  alignItems: "center",
+});
+export const dateButtonContainer = style({
+  display: "flex",
+  marginLeft: "auto",
+  gap: "10px",
+});
+export const dateInputTitle = style({
+  fontSize: 16,
+  marginRight: 10,
 });
 export const dateInputBox = style([
   {
@@ -18,7 +28,7 @@ export const dateInputBox = style([
 ]);
 
 export const dateInputButton = style({
-  width: "3rem",
+  width: "4rem",
   height: "2rem",
   fontSize: 16,
   marginTop: "1rem",
