66619a9852d17dd543a618cae9e248e9721997f8
jsleemaster <roeowo123@naver.com>
2024-02-08 17:24:26 +0900

    [fix] LCP 개선 1 suspense추가

--- src/components/lifeQuotes/Message.tsx
+++ src/components/lifeQuotes/Message.tsx
@@ -1,15 +1,19 @@
+import { Suspense } from "react";
+
 import * as styles from "./Message.css";
 
 const Message = ({ lifeQuotes }) => {
   return (
-    <article className={`${styles.quotoesBox}`}>
-      <span className={styles.quotesTitle}>
-        {lifeQuotes && lifeQuotes.title}
-      </span>
-      <span className={styles.quotesAuthor}>
-        {lifeQuotes && lifeQuotes.author}
-      </span>
-    </article>
+    <Suspense fallback={<>loading.....</>}>
+      <article className={`${styles.quotoesBox}`}>
+        <span className={styles.quotesTitle}>
+          {lifeQuotes && lifeQuotes.title}
+        </span>
+        <span className={styles.quotesAuthor}>
+          {lifeQuotes && lifeQuotes.author}
+        </span>
+      </article>
+    </Suspense>
   );
 };
 
